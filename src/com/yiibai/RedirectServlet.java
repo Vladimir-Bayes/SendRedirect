bff7d44b8f36795c10c34c0c86f4d3f6224f0202
Vladimir-Bayes <gyang.shines@gmail.com>
2017-12-26 17:15:46 +0800

    Initial commit

---
+++ src/com/yiibai/RedirectServlet.java
@@ -0,0 +1,32 @@
+package com.yiibai;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RedirectServlet
+ */
+@WebServlet("/RedirectServlet")
+public class RedirectServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 response.setContentType("text/html");
+	     PrintWriter pw = response.getWriter();
+
+	     // 直接重定向到：http://www.yiibai.com
+	     response.sendRedirect("http://www.yiibai.com");
+	     pw.close();
+	}
+
+}
---
+++ src/com/yiibai/YiiSearcher.java
@@ -0,0 +1,26 @@
+package com.yiibai;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class index
+ */
+@WebServlet("/index")
+public class YiiSearcher extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String keyword = request.getParameter("keyword");
+        response.sendRedirect("http://www.yiibai.com/search.php?kw=" + keyword);
+	}
+
+}
